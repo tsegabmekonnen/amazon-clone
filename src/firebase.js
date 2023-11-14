2fb4c6f8ec1d32e0d911b8001b86fcb9b7141470
mtsegab <m.tsegab01@gmail.com>
2023-11-14 09:41:25 -0600

    "commit 1: WIP amazon clone project"

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDjMVw1DTMofuOVjnOqTxuLTlZwqf9avU0",
+    authDomain: "challenge-3f458.firebaseapp.com",
+    projectId: "challenge-3f458",
+    storageBucket: "challenge-3f458.appspot.com",
+    messagingSenderId: "431990950174",
+    appId: "1:431990950174:web:476fb9d68ca6795821210f",
+    measurementId: "G-G0NXXBTFLM"
+};
